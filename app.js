b7a42c9dee599bb75ac8fa417b2677c9dca7ff40
Antonio2483 <antoniolsferreira2015@gmail.com>
2024-05-28 20:37:50 -0300

    esqueleto pronto

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const port = 3000;
 
 app.use(express.json());
 
-// Importa rotas
 const rotas = require('./rotas');
 
-// Usa as rotas importadas
-app.use('/professor', rotas);
+app.use('/', rotas);
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
--- rotas.js
+++ rotas.js
@@ -1,15 +1,70 @@
 const express = require('express');
 const router = express.Router();
 
-// Rota para obter dados
-router.get('/', (req, res) => {
-    res.send('Dados obtidos');
+// GET Todos os professores
+router.get('/professores', (req, res) => {
+    let dados = {};
+    let dados2 = {};
+    let dadosLista = [];
+
+    dados.nome = "Teste 1";
+    dados.matricola = "001";
+    dados.data_de_nascimento = "01/01/1980";
+
+    dados2.nome = "Teste 2";
+    dados2.matricola = "002";
+    dados2.data_de_nascimento = "01/01/1980";
+
+    dadosLista.push(dados,dados2);"|"
+
+    res.send(JSON.stringify(dadosLista));
 });
 
-// Rota para adicionar dados
-router.post('/', (req, res) => {
+// GET professor por matricola
+router.get('/professor/:matricula', (req, res) => {
+    const matricula = req.params.matricula;
+    let dados = {};
+
+    dados.nome = "Teste 1";
+    dados.matricola = "001";
+    dados.data_de_nascimento = "01/01/1980";
+
+    res.send(JSON.stringify(dados));
+});
+
+
+// POST Adicionar professor
+router.post('/professor/', (req, res) => {
     const dados = req.body;
-    res.status(201).send(`Dados adicionados: ${JSON.stringify(dados)}`);
+
+    let retorno = {};
+    retorno.status = 'Professor inserido com Sucesso!'
+
+    res.send(JSON.stringify(retorno));
+});
+
+// PATCH alterar professor por matricola
+router.patch('/professor/:matricula', (req, res) => {
+    const dados = req.body;
+    const matricula = req.params.matricula;
+
+    let retorno = {};
+    retorno.status = 'Professor alterado com Sucesso!'
+
+    res.send(JSON.stringify(retorno));
+});
+
+router.delete('/professor/:matricula', (req, res) => {
+
+    const dados = req.body;
+    const matricula = req.params.matricula;
+
+    console.log(matricula);
+
+    let retorno = {};
+    retorno.status = 'Professor excluído com Sucesso!'
+
+    res.send(JSON.stringify(retorno));
 });
 
 module.exports = router;
